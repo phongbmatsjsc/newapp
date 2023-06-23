b5f1828a43dd12777a5e2981a5a611735abea034
phongbmatsjsc <phongbm@ats.com.vn>
2023-06-23 13:44:21 +0700

    #1 Initital

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBar />
   <router-view />
 </template>
+
+<script>
+  import NavBar from "./components/NavBar.vue";
+
+  export default {
+    components: {
+      NavBar,
+    }
+  }
+</script>
 
 <style lang="scss">
 #app {
